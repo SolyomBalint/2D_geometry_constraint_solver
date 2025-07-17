22a230b53d4d75e6335dd037a6b52be4db719f14
balint.solyom <balint.solyom01@gmail.com>
2025-07-17 11:26:00 +0200

    Automatise development environment setup
    Description:
    It is important to make the development flow as easy as possible,
    especially for cpp projects. To achieve this, the setup of the
    development environment should be self-contained and automatized as
    much as possible.
    
    Solution:
    Created CONAN_HOME variable, so that conan settings and downloads can be
    found in the local project dir, not globally. Created setup script for
    conan and git environment for local project that is executed when
    entering the project with direnv.

--- devenv.nix
+++ devenv.nix
@@ -8,6 +8,8 @@ in {
   # Environmental variables
   env.GREET =
     "Welcome to 2D geometry constraint solver development environment!";
+  env.CONAN_HOME = "${config.devenv.root}/.conan2";
+
   # env.DEVENV_GCC = "${pkgs_unstable.libgcc.out}/bin/gcc";
   # env.DEVENV_GPP = "${pkgs_unstable.libgcc.out}/bin/g++";
 
@@ -19,6 +21,9 @@ in {
     -DCMAKE_TOOLCHAIN_FILE=$DEVENV_ROOT/build/Debug/generators/conan_toolchain.cmake \
     -DCMAKE_BUILD_TYPE=Debug
 
+    # This is a horrible hack, for the lack of correct clangd configuration
+    cp $DEVENV_ROOT/build/Debug/compile_commands.json $DEVENV_ROOT/build
+
     cmake --build $DEVENV_ROOT/build/Debug
   '';
 
@@ -29,6 +34,9 @@ in {
     cmake -B $DEVENV_ROOT/build/Release -S $DEVENV_ROOT \
     -DCMAKE_TOOLCHAIN_FILE=$DEVENV_ROOT/build/Release/generators/conan_toolchain.cmake \
     -DCMAKE_BUILD_TYPE=Release
+
+    # This is a horrible hack, for the lack of correct clangd configuration
+    cp $DEVENV_ROOT/build/Release/compile_commands.json $DEVENV_ROOT/build
 
     cmake --build $DEVENV_ROOT/build/Release
   '';
@@ -95,6 +103,29 @@ in {
     echo "██████╔╝██║ ╚═╝ ██║███████╗    ██║██║   ██║   "
     echo "╚═════╝ ╚═╝     ╚═╝╚══════╝    ╚═╝╚═╝   ╚═╝   "
     echo ""
-    git config commit.template .git_commit_msg_template
+
+    # Check git commit template
+    echo "Checking whether git commit template is set for local project"
+    if git config --get commit.template >/dev/null 2>&1; then
+        echo "Commit template is already set, continuing"
+    else
+        TEMPLATE_CMD="git config commit.template .git_commit_msg_template"
+        echo "No commit template configured, setting it for local project"
+        echo "Executing: $TEMPLATE_CMD"
+        $TEMPLATE_CMD
+    fi
+    echo
+
+    # Check conan profile
+    echo "Checking whether conan2 profile exists"
+    if [ -d "$CONAN_HOME" ] && [ -n "$(ls -A "$CONAN_HOME" 2>/dev/null)" ]; then
+        echo "Conan profile directory exists and contains files"
+    else
+        CONAN_CMD="conan profile detect --force"
+        echo "Directory doesn't exist or is empty, creating conan profile"
+        echo "Executing: $CONAN_CMD"
+        $CONAN_CMD
+    fi
+    echo
   '';
 }
